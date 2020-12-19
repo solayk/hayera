2577ae6a3d15adee2f892186dd5fe9b7d0be2c8c
bin <bin@DESKTOP-DVGIRB9>
2020-12-19 23:52:16 +0900

    201219_메인컨트롤러 수정

--- aHayera/src/spring/mvc/controller/ViewMainpageController.java
+++ aHayera/src/spring/mvc/controller/ViewMainpageController.java
@@ -21,35 +21,45 @@ public class ViewMainpageController {
 	
 	@RequestMapping(value="/viewTopSalesedItemName.do", method=RequestMethod.POST, produces="application/text;charset=utf-8")
 	@ResponseBody
-	public String viewTopsalesdItem(ProductVO vo) {
+	public String viewTopsalesdItemName(ProductVO vo) {
 		System.out.println("---- viewTopSalesedItemName.do 요청 됨");
 		ProductVO result = viewMainpageService.selectMaxTotalsalesProduct(vo);
 		return result.getProd_name();
-//		ArrayList<ProductVO> list = new ArrayList<ProductVO>();
-//		ProductVO prd = null;
-		/* ProductVO result = viewMainpageService.selectMaxTotalsalesProduct(vo); */
-//		ObjectVO objectVO = new ObjectVO();
-//		System.out.println("으악");
-		/* System.out.println(result.getProd_name()); */
-//		System.out.println(viewMainpageService.selectMaxTotalsalesProduct(vo).getProd_name());
-//		prd.setProd_name(viewMainpageService.selectMaxTotalsalesProduct(vo).getProd_name());
-//		prd.setBrand(viewMainpageService.selectMaxTotalsalesProduct(vo).getBrand());
-//		prd.setCapacity(result.getCapacity());
-//		prd.setDiscount_price(result.getDiscount_price());
-//		prd.setImg_url(result.getImg_url());
-//		prd.setAvg_rating(result.getAvg_rating());
-//		list.add(prd);
+		/*
+		ArrayList<ProductVO> list = new ArrayList<ProductVO>();
+		ProductVO prd = null;
+		ProductVO result = viewMainpageService.selectMaxTotalsalesProduct(vo);
+		ObjectVO objectVO = new ObjectVO();
+		System.out.println("으악");
+		System.out.println(result.getProd_name());
+		System.out.println(viewMainpageService.selectMaxTotalsalesProduct(vo).getProd_name());
+		prd.setProd_name(viewMainpageService.selectMaxTotalsalesProduct(vo).getProd_name());
+		prd.setBrand(viewMainpageService.selectMaxTotalsalesProduct(vo).getBrand());
+		prd.setCapacity(result.getCapacity());
+		prd.setDiscount_price(result.getDiscount_price());
+		prd.setImg_url(result.getImg_url());
+		prd.setAvg_rating(result.getAvg_rating());
+		list.add(prd);
 		
-//		objectVO.setList(list);
-//		System.out.println(objectVO);
-//		return objectVO;
+		objectVO.setList(list);
+		System.out.println(objectVO);
+		return objectVO;
+		*/
 	}
 	@RequestMapping(value="/viewTopSalesedItemBrand.do", method=RequestMethod.POST, produces="application/text;charset=utf-8")
 	@ResponseBody
-	public String viewPbrand(ProductVO vo) {
+	public String viewTopSalesedItemBrand(ProductVO vo) {
 		System.out.println("---- viewTopSalesedItemBrand.do 요청 됨");
 		ProductVO result = viewMainpageService.selectMaxTotalsalesProduct(vo);
 		return result.getBrand();
+	}
+	@RequestMapping(value="/viewTopSalesedItemImage.do", method=RequestMethod.POST, produces="application/text;charset=utf-8")
+	@ResponseBody
+	public String viewTopSalesedItemImage(ProductVO vo) {
+		System.out.println("---- viewTopSalesedItemImage.do 요청 됨");
+		ProductVO result = viewMainpageService.selectMaxTotalsalesProduct(vo);
+		System.out.println(result.getImg_url());
+		return result.getImg_url();
 	}
 	
 //	@RequestMapping("/viewAll.do")

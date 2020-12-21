75166d436dd84ea46d46146aa21b71e81bcab45a
hahayerasong <76099300+hahayerasong@users.noreply.github.com>
2020-12-22 00:16:30 +0900

    20201220: 마이페이지컨트롤러 주석제거

--- aHayera/src/spring/mvc/controller/MypageController.java
+++ aHayera/src/spring/mvc/controller/MypageController.java
@@ -23,17 +23,7 @@ public class MypageController {
 	@RequestMapping("/mypage.do")
 	public void getAllById(CustomerVO vo, Model m){
 		
-	/*
-	 * session값을 뷰(mypage.jsp/controller) 로그인값을 받으면 아이디가 나오니까 아이디를 가지고 디비로 가서 나머지
-	 * 정보를 다 가지고 와야함 select문 where 아이디? 로그인 후에 -> 개인정보 수정-> 로그인 정보 출력 ->로그인 id값을 갖고
-	 * 개인정보 출력 컨트롤-> 서비스-> 다오-> 메퍼 }
-	 * 
-	 * Controller의 메서드는 Model이라는 타입의 객체를 파라미터로 받을 수 있다.
-		순수하게 JSP Servlet으로 웹 어플리케이션을 만들 때 보통 request나 session 내장객체에 정보를 담아 jsp에 넘겨주곤 했는데 Spring에서는 Model이라는 녀석을 쓴다.
-		즉 request.setAttribute() 와 비슷한 역할을 하는 것.
-	 */
-		//String customerId = (String)session.getAttribute("login"); 
-		
+
 	   CustomerVO result= service.getAllById(vo);
 	   String add = result.getAddress();
 	   String[] addAry = add.split("/");

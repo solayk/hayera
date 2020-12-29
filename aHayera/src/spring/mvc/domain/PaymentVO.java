c496a66db96ad64e32563ae106f78194fdea76c5
bin <bin@DESKTOP-DVGIRB9>
2020-12-29 14:48:07 +0900

    201229_PaymentVO 추가, ObjectVO 삭제

---
+++ aHayera/src/spring/mvc/domain/PaymentVO.java
@@ -0,0 +1,26 @@
+package spring.mvc.domain;
+
+public class PaymentVO {
+	private String pay_no;
+	private String order_no;
+	private String payment_method;
+	
+	public String getPay_no() {
+		return pay_no;
+	}
+	public void setPay_no(String pay_no) {
+		this.pay_no = pay_no;
+	}
+	public String getOrder_no() {
+		return order_no;
+	}
+	public void setOrder_no(String order_no) {
+		this.order_no = order_no;
+	}
+	public String getPayment_method() {
+		return payment_method;
+	}
+	public void setPayment_method(String payment_method) {
+		this.payment_method = payment_method;
+	}
+}
